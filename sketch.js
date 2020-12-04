54e03c969fb8383c45ec5d37099dfd238f53a626
herobrine2231 <72050892+herobrine2231@users.noreply.github.com>
2020-12-04 12:25:38 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,20 +7,22 @@ const Engine = Matter.Engine;
 
 var plinkos = [];
 var divisions = [];
+var particles =[];
 
 var divisionHeight=300;
 var score =0;
 var ground;
 
 var particle;
-var gameState="play";
-var count=1;
+var gameState="play"; 
+var count=0;
 
 function setup() {
- var canvas= createCanvas(800, 800);
+  createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(width/2,height,width,20);
+
   particle=new Particle(mouseX, 10, 10, 10);
 
 
@@ -65,6 +67,18 @@ function draw() {
   textSize(20)
  
   Engine.update(engine);
+  fill("white");
+  textSize(35)
+  text(" 500 ", 5, 550);
+  text(" 500 ", 80, 550);
+  text(" 500 ", 160, 550);
+  text(" 500 ", 240, 550);
+  text(" 100 ", 320, 550);
+  text(" 100 ", 400, 550);
+  text(" 100 ", 480, 550);
+  text(" 200 ", 560, 550);
+  text(" 200 ", 640, 550);
+  text(" 200 ", 720, 550);
 
   
 
@@ -93,14 +107,14 @@ function draw() {
    }
 
    text("Score : "+score,20,30); 
-   text("Count: "+count, 20,60);
+   //text("Count: "+count, 20,60);
 
-   if(particle !== null)
+   if(particle != null)
   {
     particle.display();
     console.log(particle.body.position.x);
 
-    if (particle.body.position.y <799)
+    if (particle.body.position.y >760)
     {
       if(particle.body.position.x<300)
       {
@@ -114,15 +128,8 @@ function draw() {
         console.log(gameState);
       }
     }
-  }
 
-    if (particle!== null)
-    {
-      particle.display();
-      console.log(particle.body.position.x);
-      if (particle.body.position.y < 799)
-      {
-        if(particle.body.position.x>601 && particle.body.position.x < 799)
+       else if(particle.body.position.x>601 && particle.body.position.x < 900)
         {
           score=score+200;
           console.log(score);
@@ -133,15 +140,8 @@ function draw() {
           }
           console.log(gameState);
         }
-    }
-  }
-  if(particle!==null)
-  {
-    particle.display();
-    console.log(particle.body.position.x);
-  if (particle.body.position.y < 799)
-    {
-      if(particle.body.position.x>301 && particle.body.position.x<600)
+
+     else if(particle.body.position.x>301 && particle.body.position.x<600)
       {
         score=score+100;
         console.log(score);
@@ -153,15 +153,12 @@ function draw() {
         console.log(gameState);
       }
     }
-  }
+  
 
-  text("Score : "+score,20,30); 
+ // text("Score : "+score,20,30); 
   if(gameState ==="end")
   {
-    console.log(gameState);
-    count=0;
-    score=0;
-    console.log(count);
+    textSize(100);
     text("GAME OVER",width/2, height/2);
    
   }
